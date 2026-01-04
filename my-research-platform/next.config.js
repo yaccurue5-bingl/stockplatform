05d9c72e33c82461911ba18f1510e90dd50c1ac2
yaccurue5-bingl <yaccurue5@gmail.com>
2026-01-04 13:32:24 +0900

    new

--- my-research-platform/next.config.js
+++ my-research-platform/next.config.js
@@ -1,7 +1,7 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 정적 배포 설정
+//output: 'export', // 정적 배포 설정
   trailingSlash: true, // 주소 끝에 /를 붙여 404 방지
 };
 export default nextConfig;
